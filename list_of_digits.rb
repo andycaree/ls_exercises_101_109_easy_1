13d22294b3506c79803d0d1e6431c1305d2f1b4b
Andy <andycaree@gmail.com>
2017-02-22 23:33:30 +0000

    2nd commit

--- list_of_digits.rb
+++ list_of_digits.rb
@@ -1,5 +1,8 @@
 # Write a method that takes one argument, a positive integer,
 # and returns a list of the digits in the number.
+# I will need to split the digits up ('.split'method?)
+# this means I will need to convert the integer passed in first to a string (for split method)
+# then back to an integer
 
 def list_of_digits(int)
   integer_list = int.to_s.split('')
@@ -9,9 +12,10 @@ list_of_digits(209876)
 
 # the above works, but returns a list of strings, not integers
 
-def list_of_integers(int)
-  array = int.to_s.split('')
-  puts array.map {|int| int.to_i}
+def list_of_integers(num)
+  list = num.to_s.split('')
+  list.map {|num| num.to_i}
+  puts list
 end
 
-list_of_integers(100)
+list_of_integers(442)
