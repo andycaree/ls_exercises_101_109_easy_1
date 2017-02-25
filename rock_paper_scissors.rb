e7352d08c680c913f515e5515682e63477ceca1f
Andy <andycaree@gmail.com>
2017-02-25 20:46:40 +0000

    refactored

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -4,43 +4,50 @@
 # write a main big loop that captures almost
 #  everything to allow playing the game again
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors).freeze
 
 def greeting(message)
   puts message
 end
 
-def computing_choices(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
-    greeting('You win!')
-  elsif (player == 'scissors' && computer == 'rock') ||
-        (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors')
-    greeting('You lost!')
-  else
-    greeting("It's a tie!")
-  end
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
 end
-
-greeting('Welcome to the game!')
-
-users_choice = ''
-
-greeting("Please enter a choice to begin playing:
-        #{VALID_CHOICES.join(', ')}")
-users_choice = gets.chomp
 
 loop do
-  if VALID_CHOICES.include?(users_choice)
-    break
-  else
-    greeting('Please enter a valid choice.')
+  def computing_choices(player, computer)
+    if win?(player, computer)
+      greeting('You win!')
+    elsif win?(computer, player)
+      greeting('You lost!')
+    else
+      greeting("It's a tie!")
+    end
   end
-end
 
-computers_choice = VALID_CHOICES.sample
-greeting("Your choice is: #{users_choice}
-          and the computer chose: #{computers_choice}.")
-computing_choices(users_choice, computers_choice)
+  greeting('Welcome to the game!')
+
+  users_choice = ''
+
+  loop do
+    greeting("Please choose one: #{VALID_CHOICES.join(', ')}")
+    users_choice = gets.chomp
+
+    if VALID_CHOICES.include?(users_choice)
+      break
+    else
+      greeting('Please enter a valid choice.')
+    end
+  end
+
+  computers_choice = VALID_CHOICES.sample
+  greeting("You chose: #{users_choice} ")
+  greeting("The computer chose: #{computers_choice}")
+  computing_choices(users_choice, computers_choice)
+
+  greeting('Would you like to try again?')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
