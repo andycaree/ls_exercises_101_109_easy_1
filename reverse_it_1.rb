d4dbb976c246f2d3831598d491227b6dd7469471
Andy <andycaree@gmail.com>
2017-02-26 20:38:37 +0000

    first commit

---
+++ reverse_it_1.rb
@@ -0,0 +1,8 @@
+# Write a method that takes one argument, a string, and returns
+# the same string with the words in reverse order.
+
+def reverse(string)
+  puts string.split(" ").reverse.join(" ")
+end
+
+reverse("My name is Andy March")
