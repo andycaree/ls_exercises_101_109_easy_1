8cf99c6639ec9a61c249e9344ef44fc6f7b2e87c
Andy <andycaree@gmail.com>
2017-03-05 19:44:55 +0000

    03-05-2017

--- how_many?.rb
+++ how_many?.rb
@@ -8,7 +8,6 @@
 
 vehicles = %w(car car truck car SUV truck motorcycle motorcycle car truck)
 
-
 def count_occurences(array)
   puts "car => #{array.count('car')}"
   puts "truck => #{array.count('truck')}"
@@ -18,17 +17,50 @@ end
 
 count_occurences(vehicles)
 
-#OR...
+# OR...
 
 def count_occurrences(array)
   occurrences = {}
 
-  array.each do|array_items|
+  array.each do |array_items|
     occurrences[array_items] = array.count(array_items)
   end
 
   occurrences.each do |array_items, frequency|
     puts "#{array_items} => #{frequency}"
+  end
+end
+
+count_occurrences(vehicles)
+
+# or...
+# Write a method that counts the number of occurrences
+#  of each element in a given array.
+
+# INPUT: array
+# OUTPUT: number of occurences each element in the array occurs
+# RULES:
+
+# TEST CASES:
+# 'car' => 4
+# 'truck' => 3
+
+# DATA STRUCTURES:
+# input: array of chars
+# output: see test cases
+
+# ALGORITHM:
+# method that holds all this
+# create a variable to hold 'uniq' method results
+# print string with afore mentioned variable as well as the variable
+#   with the 'count' method run on it
+
+vehicles = %w(car car truck car SUV truck motorcycle motorcycle car truck)
+
+def count_occurrences(vehicle_array)
+  unique_vehicles = vehicle_array.uniq
+  unique_vehicles.each do |type|
+    puts "#{type} => #{vehicle_array.count(type)}"
   end
 end
 
